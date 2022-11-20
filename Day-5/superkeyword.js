4a96a99f1c13201f4e51b8976972219ada33d010
babulsarkar <babulsarkar4116@gmail.com>
2022-11-20 13:03:50 +0600

    first commit

---
+++ Day-5/superkeyword.js
@@ -0,0 +1,19 @@
+class parent{
+    hello1(){
+        console.log("Hello 1 Function");
+    }
+
+    hello2(){
+        console.log("Hello 2 Function");
+    }
+}
+
+class child extends parent{
+    demo(){
+        super.hello1();
+        super.hello2();
+    }
+}
+
+var obj=new child();
+obj.demo();
